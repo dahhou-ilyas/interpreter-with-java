a96c2a48984f86436477d62c20ac58bab2a741b7
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-05-25 14:54:08 +0100

    add tockenType

--- src/main/java/org/example/Lox.java
+++ src/main/java/org/example/Lox.java
@@ -8,8 +8,9 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class Lox {
-    public static void main(String[] args) throws IOException {
+    static boolean hadError = false;
 
+    public static void main(String[] args) throws IOException {
         if(args.length > 1){
             System.out.println("Usage: jlox [script]");
             System.exit(64);
@@ -18,12 +19,12 @@ public class Lox {
         }else {
             runPrompt();
         }
-
     }
 
     private static void runFile(String path) throws IOException {
         byte[] bytes= Files.readAllBytes(Paths.get(path));
         run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
     }
 
 
@@ -35,12 +36,25 @@ public class Lox {
             String line = reader.readLine();
             if (line == null) break;
             run(line);
+            hadError = false;
         }
     }
 
     private static void run(String source){
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
     }
 
+    static void error(int line,String message){
+        report(line,"",message);
+    }
+
+    private static void report(int line,String where,String message){
+        System.err.println(
+                "[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
 }
