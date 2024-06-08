5e545750d88192cf6b644d43c5f2d010fe223c18
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-06-09 00:52:25 +0100

    create subclass using scripts (GenerateAst)

--- src/main/java/org/tool/GenerateAst.java
+++ src/main/java/org/tool/GenerateAst.java
@@ -7,11 +7,13 @@ import java.util.List;
 
 public class GenerateAst {
     public static void main(String[] args) throws IOException {
-        if (args.length != -1){
+        if (args.length != 1){
             System.err.println("Usage: generate_ast <output directory>");
             System.exit(64);
         }
         String outputDir = args[0];
+        System.out.println(outputDir);
+
         defineAst(outputDir,"Expr", Arrays.asList(
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
