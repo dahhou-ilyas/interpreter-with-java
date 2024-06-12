4cb3362b9bd05fe62639a1ce2c8d9616aa10297e
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-06-12 05:49:47 +0100

    creating a printing to transforme AST to String

---
+++ src/main/java/org/jbox/AstPrinter.java
@@ -0,0 +1,49 @@
+package org.jbox;
+
+public class AstPrinter implements Expr.Visitor {
+    public static void main(String[] args) {
+        Expr expression=new Expr.Binary(
+                new Expr.Unary(new Token(TokenType.PLUS,"+",null,1),new Expr.Literal(1111)),
+                new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(new Expr.Literal(45.67))
+        );
+        System.out.println(new AstPrinter().print(expression));
+    }
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme,expr.left,expr.right);
+    }
+
+    @Override
+    public Object visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group",expr.expression);
+    }
+
+    @Override
+    public Object visitLiteralExpr(Expr.Literal expr) {
+        if(expr.value == null) return null;
+        return expr.value.toString();
+    }
+
+    @Override
+    public Object visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme,expr.right);
+    }
+
+    public String print(Expr expr){
+        return (String) expr.accept(this);
+    }
+
+    public String parenthesize(String name,Expr... exprs){
+        StringBuilder builder =new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr:exprs){
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+}
