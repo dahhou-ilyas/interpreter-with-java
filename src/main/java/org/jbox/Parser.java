18ad87cde4373009975eb425f2b4217be3aa678e
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-06-15 02:38:27 +0100

    add primary rule to parser

--- src/main/java/org/jbox/Parser.java
+++ src/main/java/org/jbox/Parser.java
@@ -80,6 +80,21 @@ public class Parser {
         return primary();
     }
 
+    private Expr primary(){
+        if (match(FALSE)) return new Expr.Literal(false);
+        if (match(TRUE)) return new Expr.Literal(true);
+        if (match(NIL)) return new Expr.Literal(null);
+
+        if (match(NUMBER, STRING)) {
+            return new Expr.Literal(previous().literal);
+        }
+
+        if (match(LEFT_PAREN)) {
+            Expr expr = expression();
+            consume(RIGHT_PAREN, "Expect ')' after expression.");
+            return new Expr.Grouping(expr);
+        }
+    }
     private boolean match(TokenType... types){
         for (TokenType type:types){
             if(check(type)){
