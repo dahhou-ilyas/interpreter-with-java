927e6241363e355b5d353858cb46b0e79c07d03b
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-04 02:21:36 +0100

    fix sume error

--- src/main/java/org/jbox/JBox.java
+++ src/main/java/org/jbox/JBox.java
@@ -56,17 +56,21 @@ public class JBox {
     }
 
 
-    static void error(Token token,String message){
-        if(token.type==TokenType.EOF){
-            report(token.line," at end",message);
-        }else {
-            report(token.line, " at '" + token.lexeme + "'", message);
-        }
+    static void error(int line,String message){
+        report(line,"",message);
     }
 
     private static void report(int line,String where,String message){
         System.err.println(
                 "[line " + line + "] Error" + where + ": " + message);
         hadError = true;
+    }
+
+    static void error(Token token,String message){
+        if(token.type==TokenType.EOF){
+            report(token.line," at end",message);
+        }else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 }
