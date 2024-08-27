187dcf937f9e36fa6acbd7536599d2d6b0e81e16
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-27 11:55:22 +0100

    add RunTime Error for for unary - and binary < , >, >=, <=, -, / and *

--- src/main/java/org/jbox/Interpreter.java
+++ src/main/java/org/jbox/Interpreter.java
@@ -9,16 +9,21 @@ public class Interpreter implements Expr.Visitor<Object>{
 
         switch (expr.operator.type){
             case GREATER:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left > (double) right;
             case GREATER_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left >= (double) right;
             case LESS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left < (double) right;
             case LESS_EQUAL:
+                checkNumberOperands(expr.operator, left, right);
                 return (double)left <= (double) right;
             case BANG_EQUAL: return !isEqual(left,right);
             case EQUAL_EQUAL: return isEqual(left,right);
             case MINUS:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left - (double) right;
             case PLUS:
                 if(left instanceof Double && right instanceof Double){
@@ -33,8 +38,10 @@ public class Interpreter implements Expr.Visitor<Object>{
                 }
                 break;
             case SLASH:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left / (double) right;
             case STAR:
+                checkNumberOperands(expr.operator, left, right);
                 return (double) left * (double) right;
         }
         return null;
@@ -58,9 +65,20 @@ public class Interpreter implements Expr.Visitor<Object>{
             case BANG:
                 return !isTruthy(right);
             case MINUS:
+                checkNumberOperand(expr.operator, right);
                 return - (double) right;
         }
         return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand){
+        if(operand instanceof Double) return;
+        throw new RuntimeError(operator, "operand must be a number");
+    }
+
+    private void checkNumberOperands(Token operator, Object left,Object right){
+        if(left instanceof Double && right instanceof Double) return;
+        throw new RuntimeError(operator,"Operands must be numbers.");
     }
 
     private Object evaluate(Expr expr){
---
+++ src/main/java/org/jbox/RuntimeError.java
@@ -0,0 +1,10 @@
+package org.jbox;
+
+public class RuntimeError extends RuntimeException{
+    final Token token;
+
+    RuntimeError(Token token,String message){
+        super(message);
+        this.token=token;
+    }
+}
