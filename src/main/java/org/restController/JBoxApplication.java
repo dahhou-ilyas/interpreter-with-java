b2f3bcdbd88600b5effb99db57c2e26c1eec0aa7
dahhou ilyas <dahhou.ilyas@gmail.com>
2024-08-27 13:10:29 +0100

    add spring config

---
+++ src/main/java/org/restController/JBoxApplication.java
@@ -0,0 +1,11 @@
+package org.restController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBoxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JBoxApplication.class,args);
+    }
+}
